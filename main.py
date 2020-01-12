0d095de86e0089cc64a096621e0928206dceb976
Stepan <mrstepushok@gmail.com>
2020-01-12 09:57:24 +0700

    Fixing yandex function

--- main.py
+++ main.py
@@ -378,7 +378,7 @@ class Game:
 
         def update_screen():
             background = pygame.transform.scale(load_image(
-                'background_test.jpg'), (self.WIDTH, self.HEIGHT))
+                'background_test.png'), (self.WIDTH, self.HEIGHT))
             self.screen.blit(background, (0, 0))
 
             back_image = pygame.transform.scale(
@@ -438,9 +438,9 @@ class Game:
 
         def update_screen():
             background = pygame.transform.scale(load_image(
-                'background_test.jpg'), (self.WIDTH, self.HEIGHT))
+                'background_test.png'), (self.WIDTH, self.HEIGHT))
             self.screen.blit(background, (0, 0))
-            logo = load_image('logo_test.jpg', -1)
+            logo = pygame.image.load(os.path.join('data', 'logo_blit.png'))
             self.screen.blit(logo, (200, 50))
 
         connect = Button(self.buttons_group, 340, 300, 'connect')
@@ -638,7 +638,7 @@ class Game:
 
         def update_screen():
             background = pygame.transform.scale(load_image(
-                'background_test.jpg'), (self.WIDTH, self.HEIGHT))
+                'background_test.png'), (self.WIDTH, self.HEIGHT))
             self.screen.blit(background, (0, 0))
 
         def buy_card(player, card_sprite, is_myself):
